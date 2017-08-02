e3f7b9bfa6af85fe9c3f4ac7e605ee5fe9158e62
Yan <847195183@qq.com>
2017-08-02 20:41:28 +0800

    First commit

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,17 @@
+package com.coolweather.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ////---------------
+        Toast.makeText(MainActivity.this,"alert",Toast.LENGTH_LONG).show();
+    }
+}
